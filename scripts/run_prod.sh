3445d686f7e09471096fa73ac0674945a3ef7001
Archit Khode <arkits@outlook.com>
2020-09-17 22:08:34 -0700

    [ci] fix auto deployment

--- scripts/run_prod.sh
+++ scripts/run_prod.sh
@@ -4,6 +4,14 @@ set -e
 
 echo "Running Musick!"
 
-cd /home/arkits/software/musick
+./kill.sh
+
+rm -rf ../../service
+
+mkdir -p ../../service
+
+mv musick ../service
+
+cd service
 
 ./musick > service.log 2>&1 & 
