56f4fff88a2a4e1a4abb8543b8a3754f76ad9c2d
Archit Khode <arkits@outlook.com>
2020-11-08 13:14:05 -0800

    [dev] turn off logging requests

--- handlers/logging.go
+++ handlers/logging.go
@@ -1,7 +1,6 @@
 package handlers
 
 import (
-	"log"
 	"net/http"
 	"time"
 
@@ -11,9 +10,7 @@ import (
 // LoggingMiddleware logs incoming HTTP requests
 func LoggingMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-
-		log.Printf("%s - %s | %s", r.Method, r.RequestURI, r.RemoteAddr)
-
+		// log.Printf("%s - %s | %s", r.Method, r.RequestURI, r.RemoteAddr)
 		next.ServeHTTP(w, r)
 	})
 }
